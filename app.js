66b63739184096b5f9e186df2d5255e313e44ae3
Johnl07 <118597426+Johnl07@users.noreply.github.com>
2023-06-02 19:30:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,59 @@
+window.onscroll = function() {myFunction()};
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
 
 function myFunction() {
-  var x = document.getElementById("navbar");
-  if (x.style.top === "70px"){
-    x.style.top = "0px";
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
   } else {
-    x.style.top = "70px";
+    header.classList.remove("sticky");
   }
 }
-var theme = document.getElementById('theme')
-var burger = document.getElementById('burger')
-theme.onclick = function(){
-  document.body.classList.toggle('lighttheme');
+
+
+var Skills = document.querySelector(".Skills");
+var Education = document.querySelector(".Education");
+var Experience = document.querySelector(".Experience");
+var SkillsBtn = document.querySelector(".Skills-btn");
+var EducationBtn = document.querySelector(".Education-btn");
+var ExperienceBtn = document.querySelector(".Experience-btn");
+var show;
+Education.classList.add("unshow");
+Experience.classList.add("unshow");
+
+function ShowSkills(){
+  Skills.classList.remove("unshow");
+  Experience.classList.add("unshow");
+  Education.classList.add("unshow"); 
+  ExperienceBtn.classList.remove("Hover-Effect1");
+  EducationBtn.classList.remove("Hover-Effect1"); 
+  SkillsBtn.classList.add("Hover-Effect1");
 }
 
-let bg = document.querySelector('#animation');
-
-for (var i = 0; i < 400; i++) {
-  let box = document.createElement('div');
-  box.classList.add('box');
-  bg.append(box);
+function ShowEducation(){
+  Skills.classList.add("unshow");
+  Experience.classList.add("unshow");
+  Education.classList.remove("unshow"); 
+  SkillsBtn.classList.remove("Hover-Effect1");
+  ExperienceBtn.classList.remove("Hover-Effect1");
+  EducationBtn.classList.add("Hover-Effect1"); 
 }
 
-let check = true;
-function active() {
-  if (check) {
-    hi()
-    setTimeout(hello, 600);
-    check = false;
-  } else {
-    hello()
-    setTimeout(hi, 600);
-    check = true;
-  }
+function ShowExperience(){
+  Education.classList.add("unshow");
+  Skills.classList.add("unshow");
+  Experience.classList.remove("unshow");
+  SkillsBtn.classList.remove("Hover-Effect1");
+  EducationBtn.classList.remove("Hover-Effect1");
+  ExperienceBtn.classList.add("Hover-Effect1");
+}
 
-  function hi() {
-    document.body.classList.toggle('activate');
-  }
+var Menu = document.getElementById('Menu-Button');
 
-  function hello() {
-    document.body.classList.toggle('menuX');
-  }
-
+function ShowMenu(){
+  Menu.style.left = "0px";
+}
+function UnshowMenu(){
+  Menu.style.left = "-200px";
 }
